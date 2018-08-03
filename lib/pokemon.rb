c6c66c9f317014f5f8b4697b86cb19cc89eb6c38
Christine Xiang <yutingcxiang@gmail.com>
2018-08-03 19:22:29 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -9,7 +9,7 @@ class Pokemon
   end
   
   def self.save(name, type, db)
-    db.execute("INSERT INTO pokemon (id, name, type) VALUES (?, ?, ?)", id, name, type)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?, ?)", name, type)
   end
   
   def find
